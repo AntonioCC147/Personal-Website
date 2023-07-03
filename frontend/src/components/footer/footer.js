92cba62ce6f137a8fcca14f62675994c586d5f73
Crantea Antonio-Cristian <thet0mmy1407@gmail.com>
2023-07-03 13:58:51 +0300

    Update footer.js

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -45,20 +45,6 @@ export default function App() {
                             </p>
                         </MDBCol>
 
-                        <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
-                            <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
-                            <p>
-                                <a href="#!" target="_blank" className="text-reset">
-                                Pricing
-                                </a>
-                            </p>
-                            <p>
-                                <a href="#!" target="_blank" className="text-reset">
-                                Settings
-                                </a>
-                            </p>
-                        </MDBCol>
-
                         <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
                             <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
                             <p>
