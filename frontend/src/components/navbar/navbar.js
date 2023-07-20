8f985c567a986f8f75fa3c258e193d7bb587b341
Crantea Antonio-Cristian <thet0mmy1407@gmail.com>
2023-07-20 14:59:35 +0300

    Update navbar.js

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,4 +1,6 @@
 import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -18,25 +20,27 @@ export default function NavbarHeader() {
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top" style={{marginBottom: '50px'}}>
                 <Container>
-                    <Navbar.Brand><a href="/"><img src={Icon} alt="Icon" style={{width: "35px"}}/></a></Navbar.Brand>
-                    <Navbar.Brand style={{fontWeight: "bold"}} href="/">Antonio Crantea</Navbar.Brand>
+                    <Col sm={4}>
+                        <Navbar.Brand><a href="/"><img src={Icon} alt="Icon" style={{width: "35px"}}/></a></Navbar.Brand>
+                        <Navbar.Brand style={{fontWeight: "bold"}} href="/">Antonio Crantea</Navbar.Brand>
+                    </Col>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/" className={isCurrentPage('/')}>Acasă</Nav.Link>
-                        <Nav.Link href="/despremine" className={isCurrentPage('/despremine')}>Despre mine</Nav.Link>
-                        <Nav.Link href="/portofoliu" className={isCurrentPage('/portofoliu')}>Portofoliu</Nav.Link>
-                        <Nav.Link href="/meditatii" className={isCurrentPage('/meditatii')}>Meditații</Nav.Link>
-                        <NavDropdown title="Algoritmi" id="basic-nav-dropdown" className="nav-dropdown-text">
-                            <NavDropdown.Item href="/pseudocod" className={`${isCurrentPage('/pseudocod')} navbarDropdopownText`}>Pseudocod</NavDropdown.Item>
-                                <NavDropdown.Divider/>
-                            <NavDropdown.Item href="/cpp" className={`${isCurrentPage('/cpp')} navbarDropdopownText`}>C++</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="/atestate" className={isCurrentPage('/atestate')}>Atestate</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="/contact" className={isCurrentPage('/contact')}>Contact</Nav.Link>
-                    </Nav>
+                        <Nav className="me-auto text-center">
+                            <Nav.Link href="/" className={isCurrentPage('/')}>Acasă</Nav.Link>
+                            <Nav.Link href="/despremine" className={isCurrentPage('/despremine')}>Despre mine</Nav.Link>
+                            <Nav.Link href="/portofoliu" className={isCurrentPage('/portofoliu')}>Portofoliu</Nav.Link>
+                            <Nav.Link href="/meditatii" className={isCurrentPage('/meditatii')}>Meditații</Nav.Link>
+                            <NavDropdown title="Algoritmi" id="basic-nav-dropdown" className="nav-dropdown-text">
+                                <NavDropdown.Item href="/pseudocod" className={`${isCurrentPage('/pseudocod')} navbarDropdopownText`}>Pseudocod</NavDropdown.Item>
+                                    <NavDropdown.Divider/>
+                                <NavDropdown.Item href="/cpp" className={`${isCurrentPage('/cpp')} navbarDropdopownText`}>C++</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="/atestate" className={isCurrentPage('/atestate')}>Atestate</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="/contact" className={isCurrentPage('/contact')}>Contact</Nav.Link>
+                        </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
