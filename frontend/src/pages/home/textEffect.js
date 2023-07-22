b041353e11fe310c61ea96aedaf5b238cc68534a
Crantea Antonio-Cristian <thet0mmy1407@gmail.com>
2023-07-22 17:41:11 +0300

    Update textEffect.js

--- frontend/src/pages/home/textEffect.js
+++ frontend/src/pages/home/textEffect.js
@@ -1,29 +1,29 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useMemo } from 'react';
 import './home.css';
 
 const TextEffect = () => {
-    const texts = [
+    const texts = useMemo(() => [
         "Student la Automatică și Calculatoare",
         "Îmi place Informatica și Programarea",
         "Mentor la Informatică și Matematică"
-    ];
+    ], []); // The second argument, the dependency array, is an empty array since 'texts' doesn't depend on any other variables.
+
     const [currentTextIndex, setCurrentTextIndex] = useState(0);
     const [currentText, setCurrentText] = useState(texts[0]);
 
     useEffect(() => {
         const interval = setInterval(() => {
-        setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+            setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
         }, 3000);
 
         return () => {
-        clearInterval(interval);
+            clearInterval(interval);
         };
     }, [texts]);
 
     useEffect(() => {
         setCurrentText(texts[currentTextIndex]);
-    }, [currentTextIndex]);
+    }, [texts, currentTextIndex]); // Add 'texts' as a dependency since it's used inside this effect.
 
     return (
         <div className="textEffect">
